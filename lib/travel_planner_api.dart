7c203dbe384abe6f2beed17fc96d019c40a83448
Mughees Khan <EC834571@standardbank.onmicrosoft.com>
2022-10-26 21:48:28 +0200

    Initial api

---
+++ lib/travel_planner_api.dart
@@ -0,0 +1,11 @@
+/// travel_planner_api
+///
+/// A conduit web server.
+library travel_planner_api;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:conduit/conduit.dart';
+
+export 'channel.dart';
